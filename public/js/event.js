3c96b66e237d2cedd132328d912ecaf482e9bc35
YSM <yu3096@naver.com>
2017-09-11 17:48:00 +0900

    no message

--- public/js/event.js
+++ public/js/event.js
@@ -1,10 +1,11 @@
 $(document)
     .on("contextmenu", function(e){
         e.preventDefault();
-        if( $(e.target).parent().id === "context-menu" ) {
-
+        if( $(e.target).parent()[0].id === "context-menu" ) {
+            contextMenuActions(e.target.dataset.event);
         }else{
             settingContextMenu(findIconType($(e.target)), e.pageX, e.pageY);
+            autoSelectedRadioBox($(e.target));
         }
     })
     .on("click", function(e){
@@ -52,9 +53,7 @@ function findIconType(target){
 //없을 시, form reset
 function autoSelectedRadioBox(target){
     if( target.is($("html")) ) {
-        $("#object-container").each(function(){
-           this.reset();
-        });
+        $("#object-container").find("input[type=radio][name=selector]").prop("checked", false);
     }else{
         if( target.hasClass("select-area") ) {
             target.children("input[type=radio]").prop("checked", true);
@@ -71,11 +70,38 @@ function contextMenuActions(action) {
             makeFolder("empty");
             break;
         case "delete" : //삭제
-
+            remove();
             break;
         case "open" : //열기
+            alert("개발중");
+            break;
+        case "rename" : //이름 바꾸기
+            rename();
             break;
     }
 
     settingContextMenu();
 }
+
+//input태그 foucs out // enter
+$(document).on("focusout", ".title", function(){
+    var val = $(this).val();
+    if( val.trim() === "" ){
+        //alert("폴더 / 파일 명은 공백일 수 없습니다.");
+        $(this).focus();
+
+        return;
+    }else {
+        if( $("input[type=radio][id='" + val + "']").length > 0 ){
+            //alert("폴더 / 파일 명은 중복될 수 없습니다.");
+            $(this).focus();
+
+            return;
+        }else{
+            $(this).parent().prop("for", val);
+            $(this).parent().prev().prop("id", val);
+
+            $(this).prop("readonly", true);
+        }
+    }
+});
--- public/js/functions.js
+++ public/js/functions.js
@@ -1,7 +1,9 @@
 //폴더 생성 함수
 function makeFolder(type){
     function drawFolder(dom){
-        $("#object-container").append(dom);
+        dom = $(dom);
+        dom.find(".title").val("");
+        dom.appendTo("#object-container").find(".title").prop("readonly", false).focus();
     }
 
     var url = null;
@@ -18,4 +20,14 @@ function makeFolder(type){
         success: drawFolder,
         error: alert("DOM객체 로드 중 오류가 발생하였습니다.")
     });
+}
+
+//이름 바꾸기 함수
+function rename(){
+    $("#object-container").find("input[type=radio][name=selector]:checked").parent().find("input[type=text].title").prop("readonly", false).focus();
+}
+
+//삭제 함수
+function remove(){
+    $("#object-container").find("input[type=radio][name=selector]:checked").parent().remove();
 }
